f675fa524cd56640cf42a180cadc9e5297b44f57
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-06-13 20:59:40 -0400

    we named our snake

--- client.js
+++ client.js
@@ -1,15 +1,23 @@
-
 const net = require("net");
 // This function establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '192.168.2.18',
-    port: 50541
+    host: "192.168.2.18",
+    port: 50541,
   });
+
+  
 
   conn.on("data", (data) => {
     console.log("", data);
   });
+
+  conn.on("connect", () => {
+    console.log("Connection successfully established");
+    conn.write("Name: ZP");
+  });
+
+
 
   // this inteprets incoming data as text
   conn.setEncoding("utf8");
@@ -19,4 +27,4 @@ const connect = function() {
 
 connect();
 
-module.exports = {connect};
+module.exports = { connect };
